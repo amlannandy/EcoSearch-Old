097d987fe1cebabba2a58b3195c21002aa9e37cc
Amlan <amlannandy5@gmail.com>
2021-04-16 19:03:24 +0530

    Created empty login activity

---
+++ mobile-app/app/src/main/java/com/aknindustries/ecosearch/activites/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.aknindustries.ecosearch.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aknindustries.ecosearch.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
