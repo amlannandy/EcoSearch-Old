742b634ba953e33f58f4de90d056bee25adaa62d
Amlan <amlannandy5@gmail.com>
2021-04-23 11:54:49 +0530

    Created login activity ui

--- mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/LoginActivity.kt
+++ mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/LoginActivity.kt
@@ -1,12 +1,26 @@
 package com.aknindustries.ecosearch.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aknindustries.ecosearch.R
+import com.aknindustries.ecosearch.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.loginGoToRegister.setOnClickListener { goToRegister() }
+    }
+
+    private fun goToRegister() {
+        intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
+        startActivity(intent)
     }
 }
