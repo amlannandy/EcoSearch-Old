b1c22b04b43a04b5522e8eae898b9abd81bfb30d
Amlan <amlannandy5@gmail.com>
2021-04-25 10:03:27 +0530

    Added registration validation

--- mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/RegisterActivity.kt
+++ mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/RegisterActivity.kt
@@ -1,12 +1,13 @@
 package com.aknindustries.ecosearch.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.aknindustries.ecosearch.R
 import com.aknindustries.ecosearch.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityRegisterBinding
 
@@ -17,13 +18,83 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(view)
 
         setupActionBar()
-        binding.goToLogin.setOnClickListener{ onBackPressed() }
+        binding.goToLogin.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
         setSupportActionBar(binding.toolbarRegisterActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        binding.toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
+        binding.toolbarRegisterActivity.setNavigationOnClickListener(this)
+        binding.btnRegister.setOnClickListener(this)
     }
 
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.go_to_login -> onBackPressed()
+                R.id.btn_register -> registrationHandler()
+            }
+        }
+    }
+
+    private fun registrationHandler() {
+        if (validateRegistration()) {
+            showSnackBar("Registration Valid", false)
+        }
+    }
+
+    private fun validateRegistration() : Boolean {
+        val firstName = binding.registerEtFirstName.text.toString().trim()
+        val lastName = binding.registerEtLastName.text.toString().trim()
+        val email = binding.registerEtEmail.text.toString().trim()
+        val username = binding.registerEtUsername.text.toString().trim()
+        val password = binding.registerEtPassword.text.toString().trim()
+        val confirmPassword = binding.registerEtConfirmPassword.text.toString().trim()
+        val termsAndConditions = binding.registerCbTermsAndCondition.isChecked
+        return when {
+            TextUtils.isEmpty(firstName) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+            TextUtils.isEmpty(lastName) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(username) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_username), true)
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            TextUtils.isEmpty(confirmPassword) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            // Check for valid email
+            !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_valid_email), true)
+                false
+            }
+
+            password != confirmPassword -> {
+                showSnackBar(
+                    resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),
+                    true
+                )
+                false
+            }
+            !termsAndConditions -> {
+                showSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> true
+        }
+    }
 }
