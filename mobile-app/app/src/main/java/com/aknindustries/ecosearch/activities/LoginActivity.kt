68aee1efbfe5bc53bdd452ad8edf505e74a1b671
Amlan <amlannandy5@gmail.com>
2021-04-25 08:41:21 +0530

    Added login validation

--- mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/LoginActivity.kt
+++ mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.aknindustries.ecosearch.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import com.aknindustries.ecosearch.R
 import com.aknindustries.ecosearch.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -15,8 +17,19 @@ class LoginActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.loginGoToRegister.setOnClickListener { goToRegister() }
-        binding.tvForgotPassword.setOnClickListener { goToForgotPassword() }
+        binding.loginGoToRegister.setOnClickListener(this)
+        binding.tvForgotPassword.setOnClickListener(this)
+        binding.btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.login_go_to_register -> goToRegister()
+                R.id.tv_forgot_password -> goToForgotPassword()
+                R.id.btn_login -> loginHandler()
+            }
+        }
     }
 
     private fun goToRegister() {
@@ -28,4 +41,31 @@ class LoginActivity : AppCompatActivity() {
         intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
         startActivity(intent)
     }
+
+    private fun loginHandler() {
+        val email = binding.loginEmailEt.text.toString().trim()
+        val password = binding.loginPasswordEt.text.toString().trim()
+        if (validateLogin(email, password)) {
+            showSnackBar("Valid Login", false)
+        }
+    }
+
+    private fun validateLogin(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                return false
+            }
+            TextUtils.isEmpty(password) -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                return false
+            }
+            !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                showSnackBar(resources.getString(R.string.err_msg_enter_valid_email), true)
+                false
+            }
+            else -> true
+        }
+    }
+
 }
