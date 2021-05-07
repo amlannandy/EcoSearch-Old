38caeaec256bf53de0616bcc47f68c99aa084369
Amlan <amlannandy5@gmail.com>
2021-05-07 19:35:08 +0530

    Created image frame in RecordDetailsActivity.kt

--- mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/RecordDetailsActivity.kt
+++ mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/RecordDetailsActivity.kt
@@ -1,33 +1,49 @@
 package com.aknindustries.ecosearch.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.aknindustries.ecosearch.R
 import com.aknindustries.ecosearch.api.Records
+import com.aknindustries.ecosearch.databinding.ActivityRecordDetailsBinding
 import com.aknindustries.ecosearch.models.Record
 import com.aknindustries.ecosearch.utils.Constants
+import com.aknindustries.ecosearch.utils.GlideLoader
 
-class RecordDetailsActivity : AppCompatActivity() {
+class RecordDetailsActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityRecordDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_record_details)
+        binding = ActivityRecordDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        setupActionBar()
         fetchRecord()
     }
 
     private fun fetchRecord() {
         val id = intent.getIntExtra(Constants.RECORD_ID, -1)
         if (id != -1) {
+            showProgressDialog()
             Records(applicationContext).fetchRecordById(this, id)
         }
     }
 
     fun fetchRecordSuccess(record: Record) {
-        Log.d("Record", record.title)
+        hideProgressDialog()
+        GlideLoader(applicationContext).loadRecordImage(record.image, binding.recordDetailsImageView)
+        binding.toolbarTitle.text = record.title
     }
 
     fun fetchRecordFailure(errorMessage: String) {
+        hideProgressDialog()
         Log.d("Error", errorMessage)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarRecordDetailsActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarRecordDetailsActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
