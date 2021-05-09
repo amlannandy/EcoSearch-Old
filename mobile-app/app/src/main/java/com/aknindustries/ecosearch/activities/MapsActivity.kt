10878988facc5b20fbbd9f3662d202e58ad4731d
Amlan <amlannandy5@gmail.com>
2021-05-09 18:33:00 +0530

    Added action bar in MapsActivity.kt

--- mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/MapsActivity.kt
+++ mobile-app/app/src/main/java/com/aknindustries/ecosearch/activities/MapsActivity.kt
@@ -3,6 +3,7 @@ package com.aknindustries.ecosearch.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aknindustries.ecosearch.R
+import com.aknindustries.ecosearch.databinding.ActivityMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,30 +15,29 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        setupActionBar()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarMapsActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarMapsActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
