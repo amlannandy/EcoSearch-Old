93dff849dae5f60df81b709150d0d0d8dc3b4dc2
Amlan <amlannandy5@gmail.com>
2021-06-23 19:28:31 +0530

    add react-jss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import CustomButton from './components/layout/CustomButton';
+
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <CustomButton
+        text='Test'
+        onPress={() => console.log('Test')}
+        type='primary'
+      />
     </div>
   );
 };
