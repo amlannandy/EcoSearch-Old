c1d563b81c66c7003dc6414d87d36bde537f5f49
Amlan <amlannandy5@gmail.com>
2021-06-25 11:32:13 +0530

    completed input component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/app.scss';
 
 import CustomInput from './components/layout/CustomInput';
 import CustomButton from './components/layout/CustomButton';
 
 const App = () => {
+  const [text, setText] = useState('');
+
   return (
     <div>
       <h1>Hello World</h1>
-      <CustomInput label='Hello' isBlock={true} />
+      <CustomInput
+        label='Hello'
+        isBlock={true}
+        type='password'
+        value={text}
+        onChange={(e: any) => setText(e.target.value)}
+      />
+      <CustomInput
+        label='Hello'
+        isBlock={true}
+        type='password'
+        value={text}
+        onChange={(e: any) => setText(e.target.value)}
+      />
       <CustomButton
         text='Test'
-        onClick={() => console.log('Test')}
+        onClick={() => console.log(text)}
         color='primary'
         isBlock={true}
       />
--- client/src/components/layout/CustomInput.tsx
+++ client/src/components/layout/CustomInput.tsx
@@ -2,20 +2,37 @@ import React from 'react';
 
 interface CustomInputProps {
   label: string;
+  value: string;
+  onChange: React.ChangeEventHandler;
   isBlock?: boolean;
+  type?: any;
 }
 
-const CustomInput = ({ label, isBlock = false }: CustomInputProps) => {
+const CustomInput = ({
+  label,
+  onChange,
+  value,
+  type = 'text',
+  isBlock = false,
+}: CustomInputProps) => {
   let inputClass = 'custom-input';
   if (isBlock) {
     inputClass += ' input-block';
   }
 
   return (
-    <label className={inputClass}>
-      <input type='text' placeholder=' ' />
-      <span className='placeholder'>{label}</span>
-    </label>
+    <div className='input-container'>
+      <label className={inputClass}>
+        <input
+          value={value}
+          type={type}
+          required
+          placeholder=' '
+          onChange={onChange}
+        />
+        <span className='placeholder'>{label}</span>
+      </label>
+    </div>
   );
 };
 
